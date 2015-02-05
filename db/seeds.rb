81030f0fa3fe5ec9b6aa565354b0cd5928a06682
Loadbane <joaoiep@gmail.com>
2015-02-05 23:11:40 +0800

    change db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Product.delete_all
-Product.create! id: 1, name: "Banana", price: 0.49, active: true
-Product.create! id: 2, name: "Apple", price: 0.29, active: true
-Product.create! id: 3, name: "Carton of Strawberries", price: 1.99, active: true
+Product.create! id: 1, name: "Churchill III", price: 880, active: true
+Product.create! id: 2, name: "T-127", price: 250, active: true
+Product.create! id: 3, name: "IS-6", price: 11000, active: true
 
 OrderStatus.delete_all
 OrderStatus.create! id: 1, name: "In Progress"
